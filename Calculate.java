b39f42d13ecdbc2293db8c4bfae68e97fcfd3946
Sergey Ka <mrappletasty@gmail.com>
2015-07-29 00:52:43 +0300

    first java commit

---
+++ Calculate.java
@@ -0,0 +1,8 @@
+public class Calculate{
+    public static void main(String[] arg){
+    	System.out.println("1s2");
+    	System.out.println("1s2");
+    	System.out.println("1s2");
+    	System.out.println("1s2");
+    }
+}
